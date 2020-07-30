47a1e17a488630c56a3ede684fe3aed02340021e
shutkos <shutkos@gmail.com>
2020-07-30 17:35:44 +0300

    Updates

---
+++ precache-manifest.a46d8f35d4335286f7d1e52254f4828d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2a1e470c0de2f0a419fd0eb3bffcfe",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "4da511e871f015fe6e76",
+    "url": "/goit-react-hw-01-components/static/css/main.890ce29c.chunk.css"
+  },
+  {
+    "revision": "13a030a57f3648a9d35e",
+    "url": "/goit-react-hw-01-components/static/js/2.e5c56e05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.e5c56e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da511e871f015fe6e76",
+    "url": "/goit-react-hw-01-components/static/js/main.d7f72c60.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
